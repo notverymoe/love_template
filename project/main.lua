84f534a24d694c625df56a1168c8f8150a72e7cf
Natalie <technology@notvery.moe>
2026-02-20 21:02:49 +0800

    Cleanup main.lua

--- project/main.lua
+++ project/main.lua
@@ -1,14 +1,9 @@
 -- Copyright 2026 Natalie Baker -- AGPLv3.0 --
 
-local v2d = require("lib.shrimpvec")
-
-local v = v2d(400, 300)
-
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest", 0)
-    
 end
 
 function love.draw()
-    love.graphics.print("Hello World", (v/10).x, (v*1.1).y)
+    love.graphics.print("Hello World", 100, 100)
 end
