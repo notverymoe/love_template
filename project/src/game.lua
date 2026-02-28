3668e67d66ba337181fd17590cf266ba1a473626
Natalie <technology@notvery.moe>
2026-02-28 15:53:13 +0800

    Remove beholder, evolved, shove, fixedtimer and middleclass, remove custom love.run

--- project/src/game.lua
+++ project/src/game.lua
@@ -2,26 +2,16 @@
 local log = require("lib.log")
 
 --- Setup the game
-function lovely.load()
-    log.trace("Called lovegame.update")
-end
-
---- Update the game (fixed-dt)
-function lovely.updateFixed(dt)
-    log.trace("Called lovegame.updateFixed")
+function love.load()
+    log.trace("Called love.load")
 end
 
 --- Update the game (real-dt)
-function lovely.update(dt)
-    log.trace("Called lovegame.update")
-end
-
---- Prepare frame for rendering (real-dt)
-function lovely.framePrepare(dt)
-    log.trace("Called lovegame.framePrepare")
+function love.update(dt)
+    log.trace("Called love.update")
 end
 
 --- Issue draw commands
-function lovely.frameDraw()
-    log.trace("Called lovegame.frameDraw")
+function love.draw()
+    log.trace("Called love.draw")
 end
